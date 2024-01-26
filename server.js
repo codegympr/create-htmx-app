8c66fe17e60adf7136876a6e248e8e9adddef629
Code Gym <58605923+codegympr@users.noreply.github.com>
2024-01-26 09:15:14 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Node.js/Express server setup
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes for htmx components
+app.get('/components/navbar.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'components', 'navbar.html'));
+});
+
+app.get('/components/main-content.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'components', 'main-content.html'));
+});
+
+app.get('/components/footer.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'components', 'footer.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});

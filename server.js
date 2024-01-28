01d4ea3ff7f88e53ceda1d93577ecc0259a31d00
Your Name <nodejs787@gmail.com>
2024-01-27 20:21:08 -0400

    Update package.json and remove commented code in server.js

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ app.get('/components/:componentName', (req, res) => {
     res.sendFile(path.join(__dirname, 'components', componentName));
 });
 
-// // Serve index.html for all other routes for SPA
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
 app.get('/about', (req, res) => {
     // Handle the about request
     res.sendFile(path.join(__dirname, 'src', 'components', 'about.html'));

bff2059264cd99001c5c615d345e88fe7ee7df68
Your Name <nodejs787@gmail.com>
2024-01-29 13:51:06 -0400

    Update URLs to use port 8080 instead of 3000

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,14 @@
 describe('Create HTMX App - Test Suite', () => {
   it('Successfully loads the home page', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
   })
   it('Successfully loads the about page', () => {
-    cy.visit('http://localhost:3000/about')
+    cy.visit('http://localhost:8080/about')
   })
   it('Successfully loads the contact page', () => {
-    cy.visit('http://localhost:3000/contact')
+    cy.visit('http://localhost:8080/contact')
   })
   it('Successfully loads the refresh page', () => {
-    cy.visit('http://localhost:3000/refresh')
+    cy.visit('http://localhost:8080/refresh')
   })
 })
